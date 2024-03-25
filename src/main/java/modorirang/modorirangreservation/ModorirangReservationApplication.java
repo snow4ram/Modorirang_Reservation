5f166348a1244abad064380f9ca2504a2dd074db
Joong Seok Hwang <hjs7949@naver.com>
2024-03-25 23:41:36 +0900

    🎉 init: project

---
+++ src/main/java/modorirang/modorirangreservation/ModorirangReservationApplication.java
@@ -0,0 +1,13 @@
+package modorirang.modorirangreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModorirangReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModorirangReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/modorirang/modorirangreservation/ModorirangReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package modorirang.modorirangreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModorirangReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
